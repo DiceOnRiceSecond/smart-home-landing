277489b899e5389b1fd6093da995c681bb67a249
DiceOnRiceSecond <your-email@example.com>
2025-08-10 00:11:19 +0700

    Initial smart-home landing

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Temperature from './pages/Temperature';
+
+function App(){
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/temperature" element={<Temperature/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
